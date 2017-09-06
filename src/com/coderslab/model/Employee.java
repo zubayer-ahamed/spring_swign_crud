e2815096869178b37422d7afffbd95039e0f795c
cyclingbd007 <cyclingbd007@DESKTOP-H8LRD4K>
2017-09-06 20:26:20 +0600

    new commit

---
+++ src/com/coderslab/model/Employee.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coderslab.model;
+
+/**
+ *
+ * @author cyclingbd007
+ */
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+}
